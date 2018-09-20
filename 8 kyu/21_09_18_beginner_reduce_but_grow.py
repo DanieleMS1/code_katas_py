51ecd9dfa2d7e88a995265a5c3c2efb137b9d274
DanieleMS <DanieleMS1@users.noreply.github.com>
2018-09-20 23:20:05 +0200

    .

---
+++ 8 kyu/21_09_18_beginner_reduce_but_grow.py
@@ -0,0 +1,18 @@
+# Given an array of integers (x),
+# return the result of multiplying
+# the values together in order. Example:
+#
+# [1, 2, 3] --> 6
+# For the beginner, try to use the
+# reduce method - it comes in very
+# handy quite a lot so is a good one
+# to know.
+#
+# Array will not be empty.
+
+def grow(arr):
+    pass
+
+print(grow([1, 2, 3]), 6)
+print(grow([4, 1, 1, 1, 4]), 16)
+print(grow([2, 2, 2, 2, 2, 2]), 64)
